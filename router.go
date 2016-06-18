9b0c0f020500637f4b23907aaba8998f457f29e3
Ryan Hamblin <ryanhca@gmail.com>
2016-06-18 15:59:41 -0600

    Setting up the router

---
+++ router.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+     "github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+    CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+type Router struct {}
+
+func (e *Router) ServeHTTP(w http.ResponseWriter, r *http.Request){
+    socket, err := upgrader.Upgrade(w, r, nil)
+}
+
